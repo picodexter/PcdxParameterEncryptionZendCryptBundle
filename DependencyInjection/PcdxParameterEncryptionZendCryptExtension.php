d849780c861ce75a0111c5379e3bf0289ee59ce4
picodexter <picodexter10@gmail.com>
2017-06-17 22:54:11 +0200

    Fix comment

--- DependencyInjection/PcdxParameterEncryptionZendCryptExtension.php
+++ DependencyInjection/PcdxParameterEncryptionZendCryptExtension.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the PcdxParameterEncryptionDefuseBundle package.
+ * This file is part of the PcdxParameterEncryptionZendCryptBundle package.
  *
  * (c) picodexter <https://picodexter.io/>
  *
